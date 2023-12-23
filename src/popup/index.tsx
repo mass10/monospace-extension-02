2818c79361c488789c0f49978b043bff1e9ba3f7
mass10 <mass10.github@gmail.com>
2023-12-23 14:32:06 +0900

    Update index.tsx

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,9 +1,16 @@
+/**
+ * Popup ページのエントリーポイント
+ */
+
 import '../global.css';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { proxyStore } from '../app/proxyStore';
 import Popup from './Popup';
+import { Logger } from '../utils/Logger';
+
+Logger.info('Popup page loaded');
 
 proxyStore.ready().then(() => {
   createRoot(document.getElementById('root') as HTMLElement).render(
