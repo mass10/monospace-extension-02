3a3ce92a4a80fefc2e57e996eb2fa5e403457e83
mass10 <mass10.github@gmail.com>
2023-12-23 14:31:48 +0900

    Update Options.tsx

--- src/options/Options.tsx
+++ src/options/Options.tsx
@@ -1,4 +1,14 @@
+import React from 'react';
+import { Logger } from '../utils/Logger';
+
 const Options = () => {
+  React.useEffect(() => {
+    Logger.debug('Options page loaded');
+    return () => {
+      Logger.debug('Options page unloaded');
+    };
+  }, []);
+
   return (
     <div className="flex h-screen items-center justify-center">
       <h1>Options</h1>
