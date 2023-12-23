88f5b9ec888c5e2c4d7729693f7b4339f036d956
mass10 <mass10.github@gmail.com>
2023-12-23 14:31:44 +0900

    Update index.tsx

--- src/options/index.tsx
+++ src/options/index.tsx
@@ -1,9 +1,16 @@
+/**
+ * Options ページのエントリーポイント
+ */
+
 import '../global.css';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { proxyStore } from '../app/proxyStore';
 import Options from './Options';
+import { Logger } from '../utils/Logger';
+
+Logger.info('Options page loaded');
 
 proxyStore.ready().then(() => {
   createRoot(document.getElementById('root') as HTMLElement).render(
