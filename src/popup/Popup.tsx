84e8db1be7542f364c52b509f9afb1838730d587
mass10 <mass10.github@gmail.com>
2023-12-23 14:31:53 +0900

    Update Popup.tsx

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,6 +1,15 @@
+import React from 'react';
 import { Counter } from '../app/features/counter';
+import { Logger } from '../utils/Logger';
 
 const Popup = () => {
+  React.useEffect(() => {
+    Logger.debug('Popup page loaded');
+    return () => {
+      Logger.debug('Popup page unloaded');
+    };
+  }, []);
+
   document.body.className = 'w-[30rem] h-[15rem]';
 
   return (
