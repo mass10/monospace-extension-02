fd1d3f61a2c5c6e58c6bdd850cfb9bb2ba86f8cb
mass10 <mass10.github@gmail.com>
2023-12-23 14:31:38 +0900

    Update index.tsx

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -7,8 +7,82 @@ import { debounce } from 'lodash-es';
 import { twind, config, cssom, observe, stringify } from './twind';
 import { proxyStore } from '../app/proxyStore';
 import Content from './Content';
+import { Logger } from '../utils/Logger';
 
-proxyStore.ready().then(() => {
+/**
+ * 要素のフォントを等幅に設定します。
+ *
+ * @param element HTML 要素
+ */
+function setMonospace(element: Element): void {
+  try {
+    const el = element as HTMLElement;
+    const styles = el.style;
+    if (styles.fontFamily !== "'BIZ UD明朝 Medium', monospace") {
+      styles.fontFamily = "'BIZ UD明朝 Medium', monospace";
+    }
+  } catch (error) {
+    Logger.error(error);
+  }
+}
+
+/**
+ * Google Drive の行アイテムを見やすくします。
+ */
+async function testSearchGoogleDriveItems(): Promise<void> {
+  try {
+    const driveMainPage = document.querySelector('div[id="drive_main_page"]');
+    if (!driveMainPage) {
+      // 起点となる要素が見つからない場合は何もしません。
+      return;
+    }
+
+    {
+      // ツリーアイテムの等幅(by role)
+      for (const navigation of document.querySelectorAll('nav[role="navigation"]')) {
+        for (const item of navigation.querySelectorAll('div[role="treeitem"]')) {
+          setMonospace(item);
+        }
+      }
+    }
+
+    {
+      // リストアイテムの等幅(by class)
+      for (const item of driveMainPage.querySelectorAll('div[class="KL4NAf"]')) {
+        setMonospace(item);
+      }
+    }
+
+    // {
+    //   const query = 'div[data-tooltip^="Google Drive Folder:"]';
+    //   const items = driveMainPage.querySelectorAll(query);
+    //   // const items = document.querySelectorAll(query);
+    //   for (const item of items) {
+    //     // Logger.debug(`Google Drive Folder:`, item);
+    //     setMonospace(item);
+    //   }
+    // }
+
+    // {
+    //   const query = 'div[data-tooltip^="Image:"]';
+    //   const items = driveMainPage.querySelectorAll(query);
+    //   // const items = document.querySelectorAll(query);
+    //   for (const item of items) {
+    //     // Logger.debug(`Image:`, item);
+    //     setMonospace(item);
+    //   }
+    // }
+  } catch (error) {
+    Logger.error(error);
+  }
+}
+
+function onReadyStore(): void {
+  Logger.debug(`(CONTENT READY)`);
+
+  // Google Drive の行アイテムを見やすくします。
+  window.setInterval(testSearchGoogleDriveItems, 1000 * 6);
+
   const contentRoot = document.createElement('div');
   contentRoot.id = 'my-extension-root';
   contentRoot.style.display = 'contents';
@@ -50,4 +124,22 @@ proxyStore.ready().then(() => {
       </Provider>
     </React.StrictMode>
   );
-});
+}
+
+/**
+ * Content ページのエントリーポイント
+ */
+function main(): void {
+  proxyStore
+    .ready()
+    .then(onReadyStore)
+    .catch((error) => {
+      Logger.error(error);
+    });
+
+  proxyStore.subscribe(() => {
+    Logger.debug(`(CONTENT UPDATE)`);
+  });
+}
+
+main();
