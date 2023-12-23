9ea65c01d5c486cc7b8ea601aec28e88d325e20f
mass10 <mass10.github@gmail.com>
2023-12-23 14:31:57 +0900

    Update index.tsx

--- src/welcome/index.tsx
+++ src/welcome/index.tsx
@@ -1,7 +1,14 @@
+/**
+ * Welcome ページのエントリーポイント
+ */
+
 import '../global.css';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Welcome from './Welcome';
+import { Logger } from '../utils/Logger';
+
+Logger.info('Welcome page loaded');
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
