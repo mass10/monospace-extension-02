345c92ce5853714c31ce789db4f515c554eeea57
mass10 <mass10.github@gmail.com>
2023-12-23 14:32:01 +0900

    Update Welcome.tsx

--- src/welcome/Welcome.tsx
+++ src/welcome/Welcome.tsx
@@ -1,4 +1,14 @@
+import React from 'react';
+import { Logger } from '../utils/Logger';
+
 const Welcome = () => {
+  React.useEffect(() => {
+    Logger.debug('Welcome page loaded');
+    return () => {
+      Logger.debug('Welcome page unloaded');
+    };
+  }, []);
+
   return (
     <div className="flex h-screen items-center justify-center">
       <h1>Welcome</h1>
