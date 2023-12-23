d5e1638d5bdb5e260e87978338c1ad5c455573b3
mass10 <mass10.github@gmail.com>
2023-12-23 14:31:31 +0900

    Update Content.tsx

--- src/content/Content.tsx
+++ src/content/Content.tsx
@@ -1,6 +1,19 @@
+import React from 'react';
 import { Counter } from '../app/features/counter';
+import { Logger } from '../utils/Logger';
 
 const Content = () => {
+  React.useEffect(() => {
+    Logger.debug('Content page loaded');
+    return () => {
+      Logger.debug('Content page unloaded');
+    };
+  }, []);
+
+  if (new Date().getTime()) {
+    return <></>;
+  }
+
   return (
     <div className="fixed z-[999] bottom-2 right-2 shadow-xl border-[1px] bg-white bg-opacity-10">
       <div className="flex justify-center mt-2 text-base">Content Counter</div>
